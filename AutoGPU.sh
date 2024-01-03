aa76db332ad3b6110e71e0183a443d5feba78eb1
ParrotXray <87219725+ParrotXray@users.noreply.github.com>
2024-01-03 13:44:08 +0800

    Add files via upload

--- AutoGPU.sh
+++ AutoGPU.sh
@@ -15,15 +15,15 @@ read -p "Please enter number: " cpu_choice
 case $cpu_choice in
     1)
         # Intel CPU
-        sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on"/' /etc/default/grub
+        sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on"/' /etc/default/grub
         ;;
     2)
         # AMD CPU
-        sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on"/' /etc/default/grub
+        sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on"/' /etc/default/grub
         ;;
     3)
         # Xeon CPU
-        sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt pcie_acs_override=downstream,multifunction nofb nomodeset video=vesafb:off,efifb:off"/' /etc/default/grub
+        sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt pcie_acs_override=downstream,multifunction nofb nomodeset video=vesafb:off,efifb:off"/' /etc/default/grub
         ;;
     *)
         echo "無效的選擇，請選擇1、2或3。"
@@ -46,14 +46,11 @@ echo "blacklist nvidia" >> /etc/modprobe.d/blacklist.conf
 lspci -v | grep -E "VGA compatible controller|Audio device"
 
 # 詢問使用者輸入裝置編號
-read -p "請輸入裝置編號(格式：域:總線:裝置.函數，例如 01:00.0): " device_id
+read -p "請輸入裝置編號（格式：域:總線:裝置.函數，例如 01:00.0）: " device_id
+output=$(lspci -n -s "$device_id")
 
-# 取得 PCI ID 資訊
-pci_id_info=$(lspci -n -s "$device_id" | awk '{print $3}')
-
-# 提取 Vendor ID 與 Device ID
-vendor_id=$(echo "$pci_id_info" | cut -d ':' -f 1)
-device_id=$(echo "$pci_id_info" | cut -d ':' -f 2)
+vendor_id=$(echo "$output" | awk '{print $3}' | cut -d ":" -f 1)
+device_id=$(echo "$output" | awk '{print $3}' | cut -d ":" -f 2)
 
 echo "Vendor ID: $vendor_id"
 echo "Device ID: $device_id"
